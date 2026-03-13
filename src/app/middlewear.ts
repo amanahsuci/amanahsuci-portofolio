2498a09ea99a88b2204325c6411672ca8259d780
Amanah Suci <aaamanahsc@gmail.com>
2026-03-13 19:24:39 +0700

    fix: routes

--- src/app/middlewear.ts
+++ src/app/middlewear.ts
@@ -1,12 +1,18 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
 
+    console.log('Middleware path:', pathname);
+    console.log('All cookies:', request.cookies.getAll());
+
     if (pathname.startsWith('/admin') && pathname !== '/admin') {
         const token = request.cookies.get('admin_token');
+        console.log('Token:', token);
+        
         if (!token || !token.value) {
+            console.log('No token, redirecting...');
             return NextResponse.redirect(new URL('/admin', request.url));
         }
     }
