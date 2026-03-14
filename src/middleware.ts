a8003056d2c3017432cdfc4f93f0a2f62e9f1599
Amanah Suci <aaamanahsc@gmail.com>
2026-03-14 21:29:51 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl;
-
-    if (pathname.startsWith('/admin') && pathname !== '/admin') {
-        const allCookies = request.cookies.getAll();
-        console.log('All cookies in middleware:', JSON.stringify(allCookies));
-        
-        const auth = request.cookies.get('auth');
-        console.log('Auth cookie:', auth);
-        
-        if (!auth) {
-            return NextResponse.redirect(new URL('/admin', request.url));
-        }
-    }
-
     const response = NextResponse.next();
-    response.headers.set("x-pathname", pathname);
+    response.headers.set("x-pathname", request.nextUrl.pathname);
     return response;
 }
 
